9c307d9b848fa8e1749e5946874471f7fa160505
Lukas Maximo Jardim <152202184+LukasMax95@users.noreply.github.com>
2024-09-26 20:29:24 -0300

    Validação do Kivy para o projeto Pessoal

--- PyHerd/main.py
+++ PyHerd/main.py
@@ -1,14 +1,46 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
-GUI = Builder.load_file("main.kv")
+
+class MainWindow(Screen):
+    pass
+
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
 
 
-def runapp():
-    Myapp().run()
+GUI = Builder.load_file("main0.kv")
 
 class Myapp(App):
     def build(self):
-        return GUI
+        self.root = Builder.load_file("main0.kv")
+        self.root.transition = FadeTransition()
+        self.ba1 = self.root.get_screen("first").ids["ba1"]
+        self.bb1 = self.root.get_screen("second").ids["bb1"]
+        self.ba1.bind(on_release = self.ba1_on_release)
+        self.bb1.bind(on_release = self.bb1_on_release)
+        return self.root
+
+    def ba1_on_release(self, instance):
+        if instance.text == "Send MSG":
+            self.root.current = "second"
+            self.root.transition.direction = "left"
+    
+
+    def bb1_on_release (self, instance):
+        if instance.text == "Back":
+            self.root.current = "first"
+            self.root.transition.direction = "right"
+
+
+    def on_start(self):
+        pass
+
+def runapp():
+    Myapp().run()
 
 runapp()
